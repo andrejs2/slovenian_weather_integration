efe26aa3db23b43cf5eb8aeb9f028cc4aec8ebe7
Andrej <44124120+andrejs2@users.noreply.github.com>
2025-08-29 15:50:26 +0200

    Update helpers.py

--- custom_components/slovenian_weather_integration/helpers.py
+++ custom_components/slovenian_weather_integration/helpers.py
@@ -5,19 +5,6 @@ from homeassistant.core import HomeAssistant
 
 _LOGGER = logging.getLogger(__name__)
 
-### UNUSED ###
-
-
-async def async_remove_sensors(hass: HomeAssistant, config_entry: ConfigEntry):
-    """Remove all sensors for a specific location."""
-    location = config_entry.data.get("location").lower().replace(" ", "_")
-    registry = async_get(hass)
-
-    for entity_id in list(hass.states.async_entity_ids("sensor")):
-        if entity_id.startswith(f"sensor.arso_weather_{location}"):
-            registry.async_remove(entity_id)
-            _LOGGER.info("Removed sensor: %s", entity_id)
-
 
 async def async_remove_sensors(hass: HomeAssistant, config_entry: ConfigEntry):
     """Remove sensors for a specific location."""
