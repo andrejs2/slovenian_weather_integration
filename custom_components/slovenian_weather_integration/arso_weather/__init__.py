78736d43cd6c257c33a5dcff0256157f794dfe00
Andrej <44124120+andrejs2@users.noreply.github.com>
2025-08-29 08:41:54 +0200

    Update __init__.py

--- custom_components/slovenian_weather_integration/arso_weather/__init__.py
+++ custom_components/slovenian_weather_integration/arso_weather/__init__.py
@@ -1,4 +1,16 @@
 from .client import ArsoWeather
+from .exceptions import (
+    ArsoWeatherError,
+    ArsoApiError,
+    ArsoDataError,
+)
 
-__version__ = "0.1.1"
-__all__ = ["ArsoWeather", "CurrentWeather"]
+__version__ = "0.1.0"
+
+__all__ = [
+    "ArsoWeather",
+    "CurrentWeather",
+    "ArsoWeatherError",
+    "ArsoApiError",
+    "ArsoDataError",
+]
